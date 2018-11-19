d1ef3f04345ef734f8aef484a5c0a6105d7dea89
KirillChizhov33 <42999419+KirillChizhov33@users.noreply.github.com>
2018-11-19 23:53:23 +0300

    Update main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,9 @@
 #include "Header.h"
 int main()
 {
-	List A, B;
+	List A, B; //B(A);
 	int k;
+	cout << "Original list:\n";
 	A.insertf(5);
 	A.insertf(71);
 	A.insertf(2);
@@ -10,20 +11,34 @@ int main()
 	A.insertf(33);
 	A.insertf(89);
 	A.insertf(47);
+	A.insertf(19);
+	A.insertf(56);
 	A.show();
-	B = A;
+	//B = A;
+	cout << "\n\n";
+	cout << "Can assign B = A:\n";
 	B.show();
 	k = A.max();
 	cout << "\n";
-	cout << "Can to find max element:  ";
+	cout << "Can to find max element:   ";
 	cout << "max=" << k << "\n";
 	cout << "\n\n";
-	cout << "Can insert last element\n";
-	//A.insertl(111);
+	cout << "Can insert last element: \n";
+	A.insertl(111);
+	A.show();
+	cout << "\n\n";
+	cout << "Can delete first element: \n";
+	A.deletef();
+	A.deletef();
+	A.deletef();
+	A.show();
+	cout << "\n\n";
+	cout << "Can delete last element: \n";
+	A.deletel();
+	A.deletel();
 	A.show();
 	cout << "\n";
 	cout << "Can delete all elements\n";
-	//A.~List();
+	A.~List();
 	A.show();
 }
-
